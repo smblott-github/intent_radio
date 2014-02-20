ae1157f8f9b4b44f8a84371da93f9961d3ed8a68
Stephen Blott <smblott+github@gmail.com>
2014-02-20 11:53:01 +0000

    ..

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,8 +1,12 @@
 /home/blott/local/project/intent_radio/gen/org/zapto/smblott/intentradio/R.java \
- : /home/blott/local/project/intent_radio/res/layout/main.xml \
-/home/blott/local/project/intent_radio/res/drawable-xhdpi/ic_launcher.png \
-/home/blott/local/project/intent_radio/res/drawable-hdpi/ic_launcher.png \
+ : /home/blott/local/project/intent_radio/res/values/strings.xml \
 /home/blott/local/project/intent_radio/res/drawable-mdpi/ic_launcher.png \
-/home/blott/local/project/intent_radio/res/values/strings.xml \
+/home/blott/local/project/intent_radio/res/drawable-xhdpi/ic_launcher.png \
 /home/blott/local/project/intent_radio/res/drawable-ldpi/ic_launcher.png \
+/home/blott/local/project/intent_radio/res/layout/main.xml \
+/home/blott/local/project/intent_radio/res/drawable-hdpi/ic_launcher.png \
+/home/blott/local/project/intent_radio/bin/res/drawable-mdpi/ic_launcher.png \
+/home/blott/local/project/intent_radio/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/blott/local/project/intent_radio/bin/res/drawable-ldpi/ic_launcher.png \
+/home/blott/local/project/intent_radio/bin/res/drawable-hdpi/ic_launcher.png \
 /home/blott/local/project/intent_radio/bin/AndroidManifest.xml \
