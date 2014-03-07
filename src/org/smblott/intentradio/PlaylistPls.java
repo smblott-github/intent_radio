c99e11ae57b5b49333ab25465c512646e50209ef
Stephen Blott <smblott+github@gmail.com>
2014-03-07 11:28:21 +0000

    ..

--- src/org/smblott/intentradio/PlaylistPls.java
+++ src/org/smblott/intentradio/PlaylistPls.java
@@ -6,12 +6,8 @@ import android.content.Context;
 public class PlaylistPls extends Playlist
 {
    PlaylistPls(Context context, String play_intent)
-   {
-      super(context,play_intent);
-   }
+      { super(context,play_intent); }
 
    boolean keep(String line)
-   {
-      return line.startsWith("File");
-   }
+      { return line.startsWith("File"); }
 }
