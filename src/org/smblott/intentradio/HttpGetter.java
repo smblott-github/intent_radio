253bf127a56957809538586489e304f588302de3
Stephen Blott <smblott+github@gmail.com>
2014-03-07 11:20:55 +0000

    ..

--- src/org/smblott/intentradio/HttpGetter.java
+++ src/org/smblott/intentradio/HttpGetter.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.StringBuilder;
 
-import android.util.Log;
 import java.util.List;
 import java.util.ArrayList;
 import android.text.TextUtils;
@@ -18,30 +17,23 @@ public class HttpGetter
    public static List<String> httpGet(String str)
    {
       List<String> lines = new ArrayList<String>();
-      HttpURLConnection conn;
+      HttpURLConnection conn = null;
 
       try {
          URL url = new URL(str);
          conn = (HttpURLConnection) url.openConnection();
-      }
-      catch ( Exception e ) { return lines; }
-
-      try {
          InputStream in = new BufferedInputStream(conn.getInputStream());
-         lines = readStream(in, lines);
+         readStream(in, lines);
       }
-      catch ( Exception e ) { Log.e("IntentRadio", "exception", e); }
-      finally { conn.disconnect(); }
+      catch ( Exception e ) {
+         if ( conn != null )
+            conn.disconnect();
+      }
 
       return lines;
    }
 
-   public static String httpGetStr(String str)
-   {
-      return TextUtils.join("\n", httpGet(str));
-   }
-
-   private static List<String> readStream(InputStream in, List<String> lines) throws Exception
+   private static void readStream(InputStream in, List<String> lines) throws Exception
    {
       BufferedReader fd = new BufferedReader(new InputStreamReader(in));
       String line = null;
@@ -50,14 +42,5 @@ public class HttpGetter
          lines.add(line);
 
       in.close();
-      return lines;
    }
-
-   /*
-   private static void log(String msg)
-   {
-      if ( msg != null )
-         Log.d("IntentRadio", msg);
-   }
-   */
 }
