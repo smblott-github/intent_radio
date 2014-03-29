1bf9fc122588ab6cbe17938459b6eb0989a7be1f
Stephen Blott <smblott@gmail.com>
2014-03-29 08:42:14 +0000

    Add clip button for STATE_REQUEST.

--- src/org/smblott/intentradio/ClipButtons.java
+++ src/org/smblott/intentradio/ClipButtons.java
@@ -15,6 +15,7 @@ public class ClipButtons extends Activity
    private static String intent_stop = null;
    private static String intent_pause = null;
    private static String intent_restart = null;
+   private static String intent_state_request = null;
 
    private static Context context = null;
 
@@ -29,6 +30,7 @@ public class ClipButtons extends Activity
       intent_stop = getString(R.string.intent_stop);
       intent_pause = getString(R.string.intent_pause);
       intent_restart = getString(R.string.intent_restart);
+      intent_state_request = getString(R.string.intent_state_request);
 
       setContentView(R.layout.buttons);
    }
@@ -37,10 +39,11 @@ public class ClipButtons extends Activity
     * Clip buttons...
     */
 
-   public static void clip_play(View view)    { Clipper.clip(context,intent_play); }
-   public static void clip_stop(View view)    { Clipper.clip(context,intent_stop); }
-   public static void clip_pause(View view)   { Clipper.clip(context,intent_pause); }
-   public static void clip_restart(View view) { Clipper.clip(context,intent_restart); }
+   public static void clip_play(View view)          { Clipper.clip(context,intent_play); }
+   public static void clip_stop(View view)          { Clipper.clip(context,intent_stop); }
+   public static void clip_pause(View view)         { Clipper.clip(context,intent_pause); }
+   public static void clip_restart(View view)       { Clipper.clip(context,intent_restart); }
+   public static void clip_state_request(View view) { Clipper.clip(context,intent_state_request); }
 
    /* ********************************************************************
     * Utilities...
